e7a9ac9c65e586518f33e43d47496ecbe96914fa
Haguermeister <cadethague70@gmail.com>
2022-01-12 00:07:38 -0500

    Added logic to js using jquery and bootstrap

--- README.md
+++ README.md
@@ -21,8 +21,8 @@ Weather Dashboard Project Due : 16 Jan 2022
 #### WHEN I view current weather conditions for that city
 #### THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index XXX
 #### WHEN I view the UV index
-#### THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+#### THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe XXX
 #### WHEN I view future weather conditions for that city
-#### THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+#### THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity XXX
 #### WHEN I click on a city in the search history
 #### THEN I am again presented with current and future conditions for that city
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 var loc = {
     name: "",
     lat: "",
-    lon: "",
-    ref: 0
+    lon: ""
 };
+
+var weatherHolder = JSON.parse(localStorage.getItem("weatherHolder"));
+if (weatherHolder) {
+    for (var i = 0; i < weatherHolder.length; i++) {
+        weather = weatherHolder[i];
+        addButton(weather.name);
+    }
+    $(".test").css({ backgroundColor: "#6c757d", color: "#343a40" });
+}
+else {
+    weatherHolder = [];
+
+}
+var locationHolder = [];
 var apiKey = "93f91f184c42cedf902c70f59d36a8ac";
 var apiUrl = "";
+var temp;
 
 function checkLocation() {
     var apiLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + loc.name + "&appid=" + apiKey;
@@ -21,52 +35,127 @@ function checkLocation() {
     });
 }
 function checkWeather() {
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                var weather = data;
-
-            });
-        }
-    });
+    var check = !weatherHolder.some((city) => city.name === loc.name);
+    if (check) {
+        fetch(apiUrl).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    var weather = data;
+                    var weatherHolderVar = {
+                        name: loc.name,
+                        lat: weather.lat,
+                        lon: weather.lon,
+                        current: weather.current,
+                        daily: weather.daily
+                    };
+                    locationHolder.push(weatherHolderVar.name);
+                    weatherHolder.push(weatherHolderVar);
+                    localStorage.setItem("weatherHolder", JSON.stringify(weatherHolder));
+                    $(".test").css({ backgroundColor: "#6c757d", color: "#343a40" });
+                    addButton(loc.name);
+                    displayWeather(weather);
+                });
+            }
+        });
+    }
+    else {
+        const ref = weatherHolder.findIndex((city) => city.name === loc.name);
+        weather = weatherHolder[ref];
+        $(".test").css({ backgroundColor: "#6c757d", color: "#343a40" });
+        $('button:contains(' + loc.name + ')').css({ backgroundColor: "#007bff", color: "white" });
+        displayWeather(weather);
+    }
 }
 function displayWeather(weather) {
-    console.log(weather);
-    var temp = (((weather.current.temp - 273.15) * (9 / 5)) + 32);
-    temp = temp.toFixed(2);
-    var dateObj = new Date((weather.current.dt * 1000));
-    var dateDisp = dateObj.toLocaleString([], { weekday: 'long', month: 'long', day: '2-digit', year: "numeric" })
+    var parent = document.getElementById("weatherInfo");
+    removeAllChildNodes(parent);
+    tempDisp = tempConv(weather.current.temp);
+    dateDisp = dateConv(weather.current.dt);
     var iconurl = "http://openweathermap.org/img/w/" + weather.current.weather[0].icon + ".png";
     $("#weatherInfo").append([
         $("<div>", { "class": "border border-dark mt-1" }).append([
             $("<h2>").text(loc.name + ' ' + dateDisp).append(
                 $("<img>", { "src": iconurl }))
             ,
-            $("<p>").text("Temp: " + temp + " F"),
+            $("<p>").text("Temp: " + tempDisp + " F"),
             $("<p>").text("Wind: " + weather.current.wind_speed + " MPH"),
             $("<p>").text("Humidity: " + weather.current.humidity + " %"),
-            $("<p>").text("U/V Index: " + weather.current.uvi)
+            $("<p>").text("U/V Index: ").append($("<span>", { "id": "uvSpan" }).text(weather.current.uvi))
         ]), $("<div>", { "id": "forecastHeader" }).text("5 Day Forecast").append(
             $("<div>", { "class": " dayContainer d-flex" })
         )]
     );
-    for (var i = 0; i < 5; i++) {
+    var spanColorVals = spanColor(weather.current.uvi);
+    $("#uvSpan").css("background-color", spanColorVals[0]);
+    $("#uvSpan").css("color", spanColorVals[1]);
+    for (var i = 1; i < 6; i++) {
+        var iconurl = "http://openweathermap.org/img/w/" + weather.daily[i].weather[0].icon + ".png";
+        tempDisp = tempConv(weather.daily[i].temp.day);
+        dateDisp = dateConv(weather.daily[i].dt)
         $(".dayContainer").append(
             $("<div>", { "class": "bg-dark text-white col m-1" }).append([
-                $("<p>").text("Date"),
-                $("<p>").text("Icon"),
-                $("<p>").text("Temp: " + " F"),
-                $("<p>").text("Wind: " + " MPH"),
-                $("<p>").text("Humidity: " + " %")
+                $("<p>").text(dateDisp),
+                $("<img>", { "src": iconurl }),
+                $("<p>").text("Temp: " + tempDisp + " F"),
+                $("<p>").text("Wind: " + weather.daily[i].wind_speed + " MPH"),
+                $("<p>").text("Humidity: " + weather.daily[i].humidity + " %")
             ])
         );
     }
 }
+function tempConv(temp) {
+    var tempF = (((temp - 273.15) * (9 / 5)) + 32);
+    tempF = tempF.toFixed(2);
+    return tempF;
+}
+function addButton(name) {
+    $("#citySearch").append($("<button>", { "class": "btn btn-primary mt-3 test" }).text(name).css('color', "white"));
+}
+function dateConv(utcDate) {
+    var dateObj = new Date((utcDate * 1000));
+    var dateDisp = dateObj.toLocaleString([], { year: 'numeric', month: '2-digit', day: '2-digit' })
+    return dateDisp;
+}
+function removeAllChildNodes(parent) { // https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function spanColor(uvi) {
+    if (uvi <= 2) {
+        var colors = ["green"];
+        colors.push("white");
+    }
+    else if (uvi <= 5 & uvi > 2) {
+        colors = ["yellow"];
+        colors.push("black");
+    }
+    else {
+        colors = ["red"];
+        colors.push("white");
+    }
+    return colors;
+}
 $("form").submit(function (e) {
     e.preventDefault();
     loc.name = $("#searchBar").val();
+    $("#searchBar").val('');
     loc.name = loc.name.charAt(0).toUpperCase() + loc.name.slice(1).toLowerCase();
     checkLocation();
 });
+
+$(document).on('click', '.test', function (e) {
+    loc.name = e.target.innerText;
+    const ref = weatherHolder.findIndex((city) => city.name === loc.name);
+    weather = weatherHolder[ref];
+    displayWeather(weather);
+    $(".test").css({ backgroundColor: "#6c757d", color: "#343a40" });
+    $(e.target).css({ backgroundColor: "#007bff", color: "white" });
+    checkLocation();
+});
+/* $(document).on("click", ".buttonC", function () {
+    console.log("Kelsey poops");
+
+}) */
 // function that takes city name looks at local storage for city name
 // then if not call api
