c18984c2f17047d197b4c573dae892a97e625a2a
Haguermeister <cadethague70@gmail.com>
2022-01-12 00:30:55 -0500

    Deleted example file from production code

--- assets/js/Example.js
+++
@@ -1,9 +0,0 @@
-const myArrayOfCityObjects = [{ cityName: 'Richmond' }, { cityName: 'Austin' }, { cityName: 'New York' }];
-
-const result1 = myArrayOfCityObjects.find((city) => city.cityName === 'Richmond');
-const result2 = myArrayOfCityObjects.findIndex((city) => city.cityName === 'Richmond');
-const result3 = myArrayOfCityObjects.some((city) => city.cityName === 'Richmond');
-
-console.log(result1);
-console.log(result2);
-console.log(result3);
